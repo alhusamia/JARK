a7beea4755605add848ef7697afa0edfcc617063
sarataiseer <sarataiseer97@gmail.com>
2020-05-01 16:07:08 +0200

    add

--- redux/actions/createproduct.js
+++ redux/actions/createproduct.js
@@ -1,11 +1,13 @@
 import instance from "./instance";
 
 // import { RENT } from "./types";
+import { getAllProducts } from "./allproducts";
 
 export const createProduct = (product) => async (dispatch) => {
   try {
     const res = await instance.post(`create/`, product);
     const productnew = res.data;
+    dispatch(getAllProducts());
     // console.log(product);
     // dispatch({
     //   type: RENT,
